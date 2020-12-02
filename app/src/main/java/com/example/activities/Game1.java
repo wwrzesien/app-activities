10ada31f85cff204fa4033173865b228884bc452
Wojtek <wrzesien.ww@gmail.com>
2020-12-02 17:59:14 +0100

    Created game 1 screen.

--- app/src/main/java/com/example/activities/Game1.java
+++ app/src/main/java/com/example/activities/Game1.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,11 +22,46 @@ import java.util.Map;
 public class Game1 extends AppCompatActivity {
 
     public static Map<String, List<String>> clues = new HashMap<String, List<String>>();
+    public static String whoseTurn = "Unknown";
+    public static Integer roundIterator;
+    public static Integer team1Score = 0;
+    public static Integer team2Score = 0;
+
+    public void displayClueScreen(View view) {
+        ImageView correctRef = (ImageView) findViewById(R.id.correct);
+        ImageView wrongRef = (ImageView) findViewById(R.id.wrong);
+        ImageView startRef = (ImageView) findViewById(R.id.start_game1);
+        ListView cluesListRef = (ListView) findViewById(R.id.clue_list);
+
+        startRef.animate().alpha(0f).setDuration(500);
+        cluesListRef.animate().alpha(1f).setDuration(500);
+        correctRef.animate().alpha(1f).setDuration(500);
+        wrongRef.animate().alpha(1f).setDuration(500);
+
+        Log.i("INFO", "Game1 started");
+
+
+
+//        TextView clueRef = (TextView) findViewById(R.id.clue);
+//        clueRef.setText(clues.get)
+
+    }
+
+    public void setDefaultValuesOnGameScreen() {
+        TextView turnRef = (TextView) findViewById(R.id.whose_turn);
+        turnRef.setText(whoseTurn);
+
+        TextView t1ScoreRef = (TextView) findViewById(R.id.t1_score);
+        t1ScoreRef.setText("Team A: " + team1Score);
+
+        TextView t2ScoreRef = (TextView) findViewById(R.id.t2_score);
+        t2ScoreRef.setText("Team B: " + team2Score);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game1);
+        setContentView(R.layout.display_game1);
 
 //      Read all data from Firebase
         DatabaseReference database = FirebaseDatabase.getInstance().getReference();
@@ -35,13 +74,25 @@ public class Game1 extends AppCompatActivity {
                     Map map = (Map) data.getValue();
                     clues.put(String.valueOf(map.get("name")), (List) map.get("forbidden"));
                     Log.i("Info", "" + clues);
+                    Log.i("Info", "Data retrieved from database.");
                 }
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 Log.i("Info", "onCancelled: Error: " + databaseError.getMessage());
             }
         });
+
+        setDefaultValuesOnGameScreen();
+
+//        ImageView startGame1 = (ImageView) findViewById(R.id.start_game1);
+//        startGame1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                setContentView(R.layout.display_game1);
+//                setValuesOnGameScreen(v);
+//                Log.i("Info", "Moved to screen with clues.");
+//            }
+//        });
     }
 }
